68bf247bbee46219859190de4f253df5126250f6
Shafayat Hossain <amishafayat@gmail.com>
2023-02-08 14:57:39 +0600

    index.html

--- quiz2.php
+++ quiz2.php
@@ -4,9 +4,20 @@
  * 
 */
 
-$admission_fee=0.001;
-$commission_rate=00;
-$commission=0.00;
-$commission_rate=($admission_fee>=20000)?(25):(($admission_fee>=10000 && $admission_fee<20000)?(20):(($admission_fee>=7000 && $admission_fee<10000)?(15):(0)));
-$commission=($commission_rate>0)?($admission_fee*($commission_rate/100)):('Invalid Data');
-echo $commission;
+
+
+function commission_calc($admission_fee,$fn)
+{
+    //$admission_fee=0.001;
+    $commission_rate=00;
+    $commission=0.00;
+    $commission_rate=($admission_fee>=20000)?(25):(($admission_fee>=10000 && $admission_fee<20000)?(20):(($admission_fee>=7000 && $admission_fee<10000)?(15):(0)));
+    $commission=($commission_rate>0)?($admission_fee*($commission_rate/100)):('Invalid Data');
+  //  (object) $fn;
+    exec($fn);
+}
+function print_fn($data)
+{
+    echo $data;
+}
+commission_calc(20000,'print_fn(2000)');
