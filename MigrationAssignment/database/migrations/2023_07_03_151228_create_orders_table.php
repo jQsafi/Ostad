e5e1b9764c0982720ae9fbeb6f62b4c18375f711
Shafayat Hossain <amishafayat@gmail.com>
2023-07-03 21:13:48 +0600

    Module 17 assignment

---
+++ MigrationAssignment/database/migrations/2023_07_03_151228_create_orders_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('product_id');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            // establish a foreign key relationship with the "id" column of the "products" table
+            $table->foreign('product_id')->references('id')->on('products');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
